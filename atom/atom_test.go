5189bb7ced473ea49b861a4689c6dd84536035ca
JTao <jtao539@163.com>
2022-04-26 11:49:30 +0800

    完善test

--- atom/atom_test.go
+++ atom/atom_test.go
@@ -1,12 +1,12 @@
 package atom
 
 import (
-	"github.com/jtao539/autocode/db"
+	"github.com/jtao539/autocode/util/database"
 	"testing"
 )
 
 func TestAuto(t *testing.T) {
-	db.Init("root", "admin539", "localhost", "3307", "sale")
+	database.Init("root", "admin539", "localhost", "3307", "sale")
 	a := Atom{Name: "Department", TblName: "tbl_department", Path: "..", ModName: "testimpl"}
 	a.GeneralAutoCode()
 }
