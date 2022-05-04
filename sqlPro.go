9c459edbf53128007e95f3358cfd8341155e51bb
“jtao539” <jtao539@163.com>
2022-05-04 19:07:30 +0800

    update many files

--- sqlPro.go
+++ sqlPro.go
@@ -28,19 +28,14 @@ func (s *SqlPro) SelectWithFactor(dest interface{}, request interface{}, table s
 // Update 数据更新, request为新数据的结构体, entity为SQLNULL实体 table 为表名称， 通过对比request和entity获取跳过的字段, tx 为事务支持
 func (s *SqlPro) Update(request interface{}, entity interface{}, table string, tx ...*sqlx.Tx) error {
 	var err error
-	var rows sql.Result
 	str, params := SafeUpdate(request, entity, table)
 	if len(tx) > 0 {
-		rows, err = tx[0].Exec(str, params...)
+		_, err = tx[0].Exec(str, params...)
 	} else {
-		rows, err = s.DB.Exec(str, params...)
+		_, err = s.DB.Exec(str, params...)
 	}
 	if err != nil {
 		return err
-	}
-	AffectedNum, _ := rows.RowsAffected()
-	if AffectedNum == 0 {
-		return UpdateError
 	}
 	return err
 }
@@ -48,19 +43,14 @@ func (s *SqlPro) Update(request interface{}, entity interface{}, table string, t
 // UpdateP 数据更新, request为新数据的结构体, entity为SQLNULL实体 table 为表名称， fields为需要跳过更新的字段, 通过对比request和entity获取跳过的字段, tx 为事务支持
 func (s *SqlPro) UpdateP(request interface{}, entity interface{}, table string, fields []string, tx ...*sqlx.Tx) error {
 	var err error
-	var rows sql.Result
 	str, params := SafeUpdateP(request, entity, table, fields...)
 	if len(tx) > 0 {
-		rows, err = tx[0].Exec(str, params...)
+		_, err = tx[0].Exec(str, params...)
 	} else {
-		rows, err = s.DB.Exec(str, params...)
+		_, err = s.DB.Exec(str, params...)
 	}
 	if err != nil {
 		return err
-	}
-	AffectedNum, _ := rows.RowsAffected()
-	if AffectedNum == 0 {
-		return UpdateError
 	}
 	return err
 }
