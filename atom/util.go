dcf4723400239671b8452bf3a526f3b4603e9b0f
JTao <jtao539@163.com>
2022-10-15 10:38:31 +0800

    修改文件命名规范

--- atom/util.go
+++ atom/util.go
@@ -1,9 +1,50 @@
 package atom
 
 import (
+	"bytes"
+	"log"
+	"strconv"
 	"strings"
 	"unicode"
 )
+
+type Buffer struct {
+	*bytes.Buffer
+}
+
+func NewBuffer() *Buffer {
+	return &Buffer{Buffer: new(bytes.Buffer)}
+}
+
+func (b *Buffer) Append(i interface{}) *Buffer {
+	switch val := i.(type) {
+	case int:
+		b.append(strconv.Itoa(val))
+	case int64:
+		b.append(strconv.FormatInt(val, 10))
+	case uint:
+		b.append(strconv.FormatUint(uint64(val), 10))
+	case uint64:
+		b.append(strconv.FormatUint(val, 10))
+	case string:
+		b.append(val)
+	case []byte:
+		b.Write(val)
+	case rune:
+		b.WriteRune(val)
+	}
+	return b
+}
+
+func (b *Buffer) append(s string) *Buffer {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Error in util append")
+		}
+	}()
+	b.WriteString(s)
+	return b
+}
 
 // Case2Camel 下划线写法转为大驼峰写法
 func Case2Camel(name string) string {
@@ -13,7 +54,7 @@ func Case2Camel(name string) string {
 }
 
 func Case2SmallCamel(name string) string {
-	return LowFirst(Case2SmallCamel(name))
+	return LowFirst(Case2Camel(name))
 }
 
 // LowFirst 首字母小写
@@ -22,4 +63,19 @@ func LowFirst(str string) string {
 		return string(unicode.ToLower(v)) + str[i+1:]
 	}
 	return ""
+}
+
+func Camel2Case(name string) string {
+	buffer := NewBuffer()
+	for i, r := range name {
+		if unicode.IsUpper(r) {
+			if i != 0 {
+				buffer.Append('_')
+			}
+			buffer.Append(unicode.ToLower(r))
+		} else {
+			buffer.Append(r)
+		}
+	}
+	return buffer.String()
 }
--- context.go
+++ context.go
@@ -7,7 +7,7 @@ import (
 	"os/exec"
 )
 
-const Version = "v1.0.18"
+const Version = "v1.0.19"
 
 const Api = "api"
 
